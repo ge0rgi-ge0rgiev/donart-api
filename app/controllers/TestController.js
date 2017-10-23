7e2c7bdf318c076d640862576b57123c1d55eba9
Georgi Georgiev <georgi.georgiev.vn@gmail.com>
2017-10-23 17:24:24 +0300

    Testing purposes (get domain)

--- app/controllers/TestController.js
+++ app/controllers/TestController.js
@@ -2,7 +2,10 @@ let functions = require('../libs/functions'),
     errors = require('../libs/response-errors');
 
 exports.test = (req, res, next) => {
+
+    res.sendSuccess(req.headers);
+
     // return res.sendSuccess({foo: 123});
-    throw new errors.DatabaseError();
+    // throw new errors.DatabaseError();
     // throw new SyntaxError("useful error message");
 }
